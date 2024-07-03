f68ddc3f041e2d2e036baf05df3f55af04ea4802
tanmayshende007@gmail.com <tanmayshende007@gmail.com>
2024-07-03 18:23:46 +0530

    Collision logic is added

--- index.js
+++ index.js
@@ -14,21 +14,34 @@ document.addEventListener('DOMContentLoaded', () => {
     ball.style.left = `${ballX}px`;
     ball.style.top = `${ballY}px`;
 
+    colors = ['red', 'blue', 'green', 'teal', 'rosybrown', 'tan', 'plum', 'saddlebrown'];
+
     setInterval(() => {
         ballX += dx;
         ballY += dy;
         ball.style.left = `${ballX}px`;
         ball.style.top = `${ballY}px`;
 
+        if(ballX < paddle.offsetLeft + paddle.offsetWidth &&
+            ballY >= paddle.offsetTop &&
+            ballY - ball.offsetHeight < paddle.offsetTop + paddle.offsetHeight 
+        ) {
+            dx *= -1;
+            paddle.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
+        }
+
         if(ballX > table.offsetWidth - ball.offsetWidth || ballX <= 0) dx *= -1;
         if(ballY > table.offsetHeight - ball.offsetHeight || ballY <= 0) dy *= -1;
     }, 10);
+
+    
 
 
     let paddleY = 0;
     let dpY = 10;    // displacement for paddle in y-direction
 
     document.addEventListener('keydown',(event) => {
+        event.preventDefault()
         if(event.keyCode == 38 && paddleY > 0) {
             // up arrow
             paddleY -= dpY;
@@ -38,6 +51,8 @@ document.addEventListener('DOMContentLoaded', () => {
         }
         paddle.style.top = `${paddleY}px`
     })
+
+
 
 
 })
