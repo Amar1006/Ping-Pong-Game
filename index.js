6b5564ef6850d16d8ba39f09578a04c953f7c2ca
tanmayshende007@gmail.com <tanmayshende007@gmail.com>
2024-07-04 11:17:35 +0530

    Added score board

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+    
     let ball = document.getElementById('ball'); // targetting the ball element
     let table = document.getElementById('ping-pong-table')
     let paddle = document.getElementById('paddle');
+
+    const scoreBoard = document.createElement('div');
+    scoreBoard.id = 'score-board';
+    document.body.insertBefore(scoreBoard,table)
+    
+    let score = 0;
+
+    function gameScore() {
+        let gameScore = document.getElementById('score-board');
+        
+        gameScore.textContent = `Score: ${score}`;
+    }
+
 
     // here the ballX and ballY will be helping us to set a starting point of ball w.r.t table
     let ballX = 50; // distance of the top of the ball w.r.t ping pong table
@@ -14,7 +28,7 @@ document.addEventListener('DOMContentLoaded', () => {
     ball.style.left = `${ballX}px`;
     ball.style.top = `${ballY}px`;
 
-    colors = ['red', 'blue', 'green', 'teal', 'rosybrown', 'tan', 'plum', 'saddlebrown'];
+    colors = ['red', 'blue', 'green', 'teal', 'rosybrown', 'tan', 'plum', 'saddlebrown','yellow', 'brown', 'grey'];
 
     setInterval(() => {
         ballX += dx;
@@ -22,16 +36,22 @@ document.addEventListener('DOMContentLoaded', () => {
         ball.style.left = `${ballX}px`;
         ball.style.top = `${ballY}px`;
 
+        //  Collision Logic
+
         if(ballX < paddle.offsetLeft + paddle.offsetWidth &&
             ballY >= paddle.offsetTop &&
             ballY - ball.offsetHeight < paddle.offsetTop + paddle.offsetHeight 
         ) {
             dx *= -1;
+            score += 10;
             paddle.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
         }
 
+        // Change in ball direction
+
         if(ballX > table.offsetWidth - ball.offsetWidth || ballX <= 0) dx *= -1;
         if(ballY > table.offsetHeight - ball.offsetHeight || ballY <= 0) dy *= -1;
+        gameScore();
     }, 10);
 
     
@@ -51,8 +71,6 @@ document.addEventListener('DOMContentLoaded', () => {
         }
         paddle.style.top = `${paddleY}px`
     })
-
-
 
 
 })
